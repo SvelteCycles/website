9af73583384d03a910154a0df150af88f1b2d737
NicolePell <pellicena@gmail.com>
2015-05-13 05:36:53 +0100

    still with Actionmailer

--- config/environment.rb
+++ config/environment.rb
@@ -5,12 +5,13 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.smtp_settings = {
    :tls => true,
    :address => "smtpout.europe.secureserver.net",
    :port => 80,
    :domain => "www.svelte-cycles.com",
-   :authentication => :login,
+   :authentication => :plain,
    :user_name => ENV["MAILER_EMAIL"],
    :password => ENV["MAILER_PASSWORD"]
  }
