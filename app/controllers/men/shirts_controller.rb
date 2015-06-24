c15c314ca1604efc6c5de2c6eeccbc012d9587f5
NicolePell <pellicena@gmail.com>
2015-06-24 21:36:57 +0100

    fixed error on mens shirts controller

--- app/controllers/men/shirts_controller.rb
+++ app/controllers/men/shirts_controller.rb
@@ -2,7 +2,7 @@ class Men::ShirtsController < ApplicationController
 
   def index
     @mens = Gender.find_by_sex('male')
-    @shirts = Category.find_by_category('baselayers')
+    @shirts = Category.find_by_category('shirts')
     @mens_shirts = Inventory.where(product_id: (Product.where(category_id: @shirts.id)), gender_id: @mens.id, size_id: 1)
   end
 
